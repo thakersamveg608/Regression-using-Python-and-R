a13511d78ed154bf91ea6d3f0bb5e1ce0102ac0b
thakersamveg608 <thakersamveg608@gmail.com>
2018-01-26 22:53:12 +0530

    Linear,Multiple,Polynomial,Support Vector Regression

---
+++ Section 4 - Simple Linear Regression/Simple_Linear_Regression/simple_linear_regression.R
@@ -0,0 +1,41 @@
+#Importing the dataset
+dataset  = read.csv('Salary_Data.csv')
+
+#Building training set and test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset,split == TRUE)
+test_set = subset(dataset,split == FALSE)
+
+#Fitting Simple Linear Regression to the Training set
+regressor = lm(formula = Salary ~ YearsExperience,
+               data = training_set)
+
+#Predicting the test results
+y-pred = predict(regressor , newdata = test_set)
+
+#Visualising the training set results
+# install.packages("ggplot2")
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = training_set$YearsExperience,y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience , predict(regressor , newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience(Training set)') +
+  xlab('Years of Experience') +
+  ylab('Salary')
+
+#Visualising the test set results
+# install.packages("ggplot2")
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = test_set$YearsExperience,y = test_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience , predict(regressor , newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience(Training set)') +
+  xlab('Years of Experience') +
+  ylab('Salary')
---
+++ Section 4 - Simple Linear Regression/Simple_Linear_Regression/simple_linear_regression.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 12:09:56 2018
+
+@author: samveg
+"""
+
+#Simple Linear Regression
+
